62fbfc58ef6541ab7d26e041531193724dd3dd47
Sanny Do <sanny.do94@gmail.com>
2020-08-19 03:52:59 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -4,18 +4,13 @@ end
 
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
- person1 = {
-    :location => "New York City",
-    :occupation => "Student",
-  }
-  person2 = {
-    :location => "New York City",
-    :occupation => "Cashier",
-  }
-person1 
+ hash[key]
 end
 
 def update_counting_hash(hash, key)
-  hash_to_change = {apples: 3}
-  updating_counting_hash = hash_to_change += 1
+  if hash[key]
+    hash[key] = hash[key] + 1
+  else
+    hash[key] = 1
+  end
 end
