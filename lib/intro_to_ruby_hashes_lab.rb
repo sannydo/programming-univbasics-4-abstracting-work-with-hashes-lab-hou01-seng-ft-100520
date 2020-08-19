9984549352cc95180c83e43bbb5f6830dea83a0d
Sanny Do <sanny.do94@gmail.com>
2020-08-19 03:55:59 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -8,9 +8,10 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-   hash[key] = hash[key] += 1 :
-    
-  hash[key] = 1
-   hash
-  end
+ if hash[key]
+   hash[key] = hash[key] + 1
+ else
+   hash[key] = 1
+ end
+ hash
 end
